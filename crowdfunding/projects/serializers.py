82f85596fad78dc304f4aa59cdfe4da4bf4fdf0a
Albany276 <albany.alfonzo@hotmail.com>
2020-09-08 20:43:57 +1000

    up to pledges
    went through crowdfunding setup and crowdfunding handling relationships pdfs successfully

---
+++ crowdfunding/projects/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Project
+from .models import Pledge
+
+
+class PledgeSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    amount = serializers.IntegerField()
+    comment = serializers.CharField(max_length=200)
+    anonymous = serializers.BooleanField()
+    supporter = serializers.CharField(max_length=200)
+    project_id = serializers.IntegerField() #cannot hand over the project object
+
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
+
+
+class ProjectSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    title = serializers.CharField(max_length=200)
+    description = serializers.CharField(max_length=None) 
+    goal = serializers.IntegerField()
+    image = serializers.URLField()
+    is_open = serializers.BooleanField()
+    date_created = serializers.DateTimeField()
+    owner = serializers.CharField(max_length=200)
+    
+
+    def create(self, validated_data):
+        return Project.objects.create(**validated_data)
+
+class ProjectDetailSerializer(ProjectSerializer): #inherits everything from ProjectSerializer class above. We are creating this so the pledges info is only avail when you access the project details view
+    pledges = PledgeSerializer(many=True, read_only=True) #read_only so when you are viewing the project you can also view the pledges but you cannot change the pledges
+
